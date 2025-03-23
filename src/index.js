1074ec61bd9ea3b736e4ed7035a1d0e936b7e32a
Jeff <jeff.wafula1@student.moringaschool.com>
2025-03-23 11:09:51 +0300

    after declaration of const

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () =>){
+    const characterBar = document.getElementById("character-Bar");
+    const detailedInfo = document.getElementById("detailed-info");
+    const votesForm = document.getElementById("votes-form");
+    const votesInput = document.getElementById("votes");
+    const resetForm = document.getElementById("reset-btn");
+} 
